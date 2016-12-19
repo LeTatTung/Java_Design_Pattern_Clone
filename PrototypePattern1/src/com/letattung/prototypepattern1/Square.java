95a215ac23d56c5333fb24a4a0c5b443795e4963
Le Tat Tung <letattungtb@gmail.com>
2016-12-19 14:22:15 +0700

    Prototype Pattern Tutorial

---
+++ PrototypePattern1/src/com/letattung/prototypepattern1/Square.java
@@ -0,0 +1,16 @@
+// hoc theo java design pattern
+package com.letattung.prototypepattern1;
+
+public class Square extends Shape{
+
+	public Square() {
+		// TODO Auto-generated constructor stub
+		type = "Square";
+	}
+	@Override
+	void draw() {
+		// TODO Auto-generated method stub
+		System.out.println("Inside Square::draw() method.");
+	}
+
+}
