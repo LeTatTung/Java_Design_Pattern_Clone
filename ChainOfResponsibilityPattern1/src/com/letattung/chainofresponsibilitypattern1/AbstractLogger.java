4011a6662dda1e4ec4b89bf3c3c1fd7d2430676d
Le Tat Tung <letattungtb@gmail.com>
2016-12-25 13:00:20 +0700

    Chain of Responsibility Pattern Tutorial

---
+++ ChainOfResponsibilityPattern1/src/com/letattung/chainofresponsibilitypattern1/AbstractLogger.java
@@ -0,0 +1,30 @@
+// hoc theo java design pattern tutorial
+
+package com.letattung.chainofresponsibilitypattern1;
+
+public abstract class AbstractLogger {
+
+	public static int INFO = 1;
+	public static int DEBUG = 2;
+	public static int ERROR = 3;
+	
+	protected int level;
+	
+	//next element in chain or responsibility
+	protected AbstractLogger nextLogger;
+	
+	public void setNextLogeer(AbstractLogger nextLogger){
+		this.nextLogger = nextLogger;
+	}
+	
+	public void logMessage(int level, String message){
+		if (this.level <= level){
+			write(message);
+		}
+		if (nextLogger != null){
+			nextLogger.logMessage(level, message);
+		}
+	}
+	
+	abstract protected void write(String message);
+}
---
+++ ChainOfResponsibilityPattern1/src/com/letattung/chainofresponsibilitypattern1/FileLogger.java
@@ -0,0 +1,18 @@
+// hoc theo java design pattern tutorial
+
+package com.letattung.chainofresponsibilitypattern1;
+
+public class FileLogger extends AbstractLogger{
+
+	public FileLogger(int level) {
+		// TODO Auto-generated constructor stub
+		this.level = level;
+	}
+	
+	@Override
+	protected void write(String message) {
+		// TODO Auto-generated method stub
+		System.out.println("File::Logger: " + message);
+	}
+
+}
