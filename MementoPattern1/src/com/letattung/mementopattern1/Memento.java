eb944153e9f5a5408458b45b72e346e8774e3694
Le Tat Tung <letattungtb@gmail.com>
2016-12-27 16:19:58 +0700

    Memento Pattern Turorial

---
+++ MementoPattern1/src/com/letattung/mementopattern1/Memento.java
@@ -0,0 +1,17 @@
+// hoc theo java design pattern tutorial
+
+package com.letattung.mementopattern1;
+
+public class Memento {
+
+	private String state;
+	
+	public Memento(String state) {
+		// TODO Auto-generated constructor stub
+		this.state = state;
+	}
+	
+	public String getState(){
+		return state;
+	}
+}
