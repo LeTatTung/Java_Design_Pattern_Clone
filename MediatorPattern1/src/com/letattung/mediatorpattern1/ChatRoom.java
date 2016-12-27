095de12e5d109219dee96387ba006a75d26dd73c
Le Tat Tung <letattungtb@gmail.com>
2016-12-27 15:38:28 +0700

    Mediator Pattern Tutorial

---
+++ MediatorPattern1/src/com/letattung/mediatorpattern1/ChatRoom.java
@@ -0,0 +1,12 @@
+// hoc theo java design pattern tutorial
+
+package com.letattung.mediatorpattern1;
+
+import java.util.Date;
+
+public class ChatRoom {
+
+	public static void showMessage(User user,String message){
+		System.out.println(new Date().toString() + " [" + user.getName() + "] : " + message);
+	}
+}
