cd531d1c32c13af0008ada36742d3e24c70520e0
Le Tat Tung <letattungtb@gmail.com>
2016-12-30 21:57:34 +0700

    Strategy Pattern Tutorial

---
+++ StrategyPattern1/src/com/letattung/strategypattern1/OperationAdd.java
@@ -0,0 +1,13 @@
+// hoc theo java design pattern tutorial
+
+package com.letattung.strategypattern1;
+
+public class OperationAdd implements Strategy{
+
+	@Override
+	public int doOperation(int num1, int num2) {
+		// TODO Auto-generated method stub
+		return num1 + num2;
+	}
+
+}
---
+++ StrategyPattern1/src/com/letattung/strategypattern1/Strategy.java
@@ -0,0 +1,8 @@
+// hoc theo java design pattern tutorial
+
+package com.letattung.strategypattern1;
+
+public interface Strategy {
+
+	public int doOperation(int num1, int num2);
+}
