33306c1d516a00518ddbcf131b7f08a83ea7b99d
Le Tat Tung <letattungtb@gmail.com>
2016-12-31 11:29:45 +0700

    MVC Pattern Tutorial

---
+++ MVCPattern1/src/com/letattung/MVCpattern1/StudentView.java
@@ -0,0 +1,13 @@
+// hoc theo java design pattern tutorial
+
+package com.letattung.MVCpattern1;
+
+public class StudentView {
+
+	public void printStudentDetails(String studentRollNo, String studentName){
+		
+		System.out.println("Student: ");
+		System.out.println("Roll No: " +studentRollNo);
+		System.out.println("Name: " +studentName);
+	}
+}
