36e1845a6582e7ca25cfe53cede05df9e92c8535
Le Tat Tung <letattungtb@gmail.com>
2017-01-21 16:23:35 +0700

    Monad

---
+++ Monad/src/com/letattung/src/Sex.java
@@ -0,0 +1,9 @@
+/*
+ * hoc theo java design pattern cua iluwatar
+ * https://github.com/iluwatar/java-design-patterns/tree/master/monad
+ */
+package com.letattung.src;
+
+public enum Sex {
+	MALE, FEMALE
+}
