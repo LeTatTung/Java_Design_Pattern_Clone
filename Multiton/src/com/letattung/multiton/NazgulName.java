e3f7ec956b16cfaf01fe04f9d1db6d8904f1f4a7
Le Tat Tung <letattungtb@gmail.com>
2017-02-06 09:40:52 +0700

    Multiton

---
+++ Multiton/src/com/letattung/multiton/NazgulName.java
@@ -0,0 +1,10 @@
+/*
+ * hoc theo java design pattern cua iluwatar
+ * https://github.com/iluwatar/java-design-patterns/tree/master/multiton
+ */
+package com.letattung.multiton;
+
+public enum NazgulName {
+	
+	KHAMUL, MURAZOR, DWAR, JI_INDUR, AKHORAHIL, HOARMURATH, ADUNAPHEL, REN, UVATHA;
+}
