23c09619c5a879c6b33ab92395d6671adf0236d9
Le Tat Tung <letattungtb@gmail.com>
2017-02-18 10:29:54 +0700

    nio channel of framework (reactor)

---
+++ Reactor/src/com/letattung/reactor/framework/ChangeKeyOpsCommand.java
@@ -0,0 +1,23 @@
+package com.letattung.reactor.framework;
+
+import java.nio.channels.SelectionKey;
+
+public class ChangeKeyOpsCommand implements Runnable{
+
+	private final SelectionKey key;
+	private final int interestedOps;
+	
+	public ChangeKeyOpsCommand(SelectionKey key, int interestedOps) {
+		this.key = key;
+		this.interestedOps = interestedOps;
+	}
+	
+	public void run(){
+		key.interestOps(interestedOps);
+	}
+	
+	@Override
+	public String toString(){
+		return "Change of ops to: " + interestedOps;
+	}
+}
---
+++ Reactor/src/com/letattung/reactor/framework/Dispatcher.java
@@ -0,0 +1,10 @@
+package com.letattung.reactor.framework;
+
+import java.nio.channels.SelectionKey;
+
+public interface Dispatcher {
+
+	void onChannelReadEvent(AbstractNioChannel channel, Object readObject, SelectionKey key);
+	
+	void stop() throws InterruptedException;
+}
