4298e0a593852151393b99234826bc8d339972a8
Le Tat Tung <letattungtb@gmail.com>
2017-02-27 22:14:24 +0700

    double-checked-locking

---
+++ double-checked-locking/src/com/letattung/dcl/Item.java
@@ -0,0 +1,9 @@
+/*
+ * hoc theo java design pattern cua iluwatar
+ * https://github.com/iluwatar/java-design-patterns/tree/master/double-checked-locking
+ */
+package com.letattung.dcl;
+
+public class Item {
+
+}
